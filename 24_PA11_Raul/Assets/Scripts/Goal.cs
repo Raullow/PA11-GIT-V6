f5c1f2ebe8aa0e80e1ebc31710d5bf26c5b31ce3
Raullow <64115106+Raullow@users.noreply.github.com>
2020-06-18 13:49:38 +0800

    Updated by Raul

---
+++ 24_PA11_Raul/Assets/Scripts/Goal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Goal : MonoBehaviour
+{
+    public Text Txt_Score;
+    public int Score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Obstacle")
+        {
+            Score++;
+            Txt_Score.text = "Score : " + Score;
+        }
+    }
+}
---
+++ 24_PA11_Raul/Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+
+        transform.position = new
+        Vector3(Mathf.Clamp(transform.position.x, -8.4f, -8.4f), Mathf.Clamp(transform.position.y, -4f, 4f), transform.position.z);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
